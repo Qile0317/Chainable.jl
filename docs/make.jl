16993fd289cfbaded1201c8b48689dcfd874c1b8
Qile0317 <qile0317@gmail.com>
2024-12-31 13:50:02 +0800

    more docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,8 @@
 push!(LOAD_PATH,"../src/")
+
 using Documenter, Chainable
 
-makedocs(sitename="Chainable.jl")
+makedocs(sitename="Chainable.jl", modules = [Chainable])
 
 deploydocs(;
     repo="github.com/Qile0317/Chainable.jl.git", branch = "gh-pages", devbranch = "main"
--- src/Chainable.jl
+++ src/Chainable.jl
@@ -1,5 +1,7 @@
 module Chainable
 
+@doc read(joinpath(dirname(@__DIR__), "README.md"), String) Chainable
+
 include("chainable.jl")
 
 end # module Chainable
